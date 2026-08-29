970064906278e926f1555860e42a69dd3a23d645
Ivan Li <ivanli2048@gmail.com>
2026-08-29 23:02:21 +0800

    test(schema): include latest reconciliation migration
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- src/tests/schema_migrations.rs
+++ src/tests/schema_migrations.rs
@@ -956,7 +956,7 @@ async fn baseline_adoption_records_compatible_existing_schema_without_full_boots
     assert_eq!(
         versions,
         vec![
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
         ]
     );
 
